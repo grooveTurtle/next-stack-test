4270d36db31478b450c15cdacc35eb6f4841678d
Zess <zess@inven.co.kr>
2025-01-08 16:58:18 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    // domains: ["https://shopping-phinf.pstatic.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "shopping-phinf.pstatic.net",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
