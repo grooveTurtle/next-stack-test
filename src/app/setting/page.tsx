1247e46fb1fa409298f4fe39be8d5ddf73749098
grooveT <grooveturtle29@gmail.com>
2025-01-09 15:51:13 +0900

    즉시 시전 스킬 시전시

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import keyBindingList from "@/mock/keyOptions.json";
+import style from "./temp.module.css";
 
 // FIXME: 파일 분리 필요
 type KeyBinding = {
@@ -9,6 +10,8 @@ type KeyBinding = {
   name: string;
   target: boolean;
   cooltime: number;
+  remainingCooltime: number;
+  image: string;
 };
 
 export default function Page() {
@@ -19,27 +22,37 @@ export default function Page() {
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
+      // 키 유효성 검증
+      const currentKey = keyBindingList.find(
+        (data) => String(data.key) === event.key
+      );
+
+      console.log("currentKey", currentKey);
+
       if (!isKeyPressed) {
         // 키 유효성 검증
-        const currentKey = keyBindingList.find((data) => {
-          return String(data.key) === event.key;
-        });
         if (currentKey) {
           setIsKeyPressed(true);
           setKeySequence(currentKey);
 
           // 즉발 스킬은 바로 프로세스 실행
           if (!currentKey.target) {
+            setIsKeyPressed(false);
+            setKeySequence(null); // 상태 초기화
             runSpecificProcess(currentKey);
           }
         }
       } else {
-        // 스킬 단축키를 누른 이후 Enter 키가 눌렸을 때
-        if (event.key === "Enter") {
-          console.log("Enter key pressed after:", keySequence);
+        if (event.key === "Escape") {
           setIsKeyPressed(false);
           setKeySequence(null); // 상태 초기화
-          // 여기서 특정 프로세스를 실행
+          return;
+        }
+
+        // 스킬 단축키를 누른 이후 Enter 키가 눌렸을 때
+        if (event.key === "Enter") {
+          setIsKeyPressed(false);
+          setKeySequence(null); // 상태 초기화
           runSpecificProcess(keySequence);
         }
       }
@@ -50,27 +63,46 @@ export default function Page() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [isKeyPressed, keySequence]);
+  }, [isKeyPressed, keySequence, eventList]);
 
-  const runSpecificProcess = (target: KeyBinding | null) => {
-    if (!target) return;
+  const runSpecificProcess = (skill: KeyBinding | null) => {
+    if (!skill || !(skill.cooltime > 0)) return;
+
+    if (eventList.some((item) => item.key === skill.key)) {
+      console.log(`이미 실행 중입니다.`);
+      return;
+    }
 
     // 이벤트 리스트 변수에 해당 항목 추가
-    setEventList((prevList) => [...prevList, target]);
+    setEventList((prevList) => [
+      ...prevList,
+      { ...skill, remainingCooltime: skill.cooltime },
+    ]);
 
-    // 스킬 아이콘 출력
-    console.log(`Skill icon for ${target.name} displayed`);
+    // 매 초 마다 target.remainingCooltime을 갱신해줘
+    const intervalId = setInterval(() => {
+      setEventList((prevList) =>
+        prevList.map((item) =>
+          item.key === skill.key
+            ? { ...item, remainingCooltime: item.remainingCooltime - 1 }
+            : item
+        )
+      );
+    }, 1000);
+
+    const cooltimeDuration = skill.cooltime * 1000;
 
     // 쿨타임 적용 (종료시 이벤트리스트 변수에서 제거)
     setTimeout(() => {
+      clearInterval(intervalId);
       setEventList((prevList) =>
-        prevList.filter((item) => item.key !== target.key)
+        prevList.filter((item) => item.key !== skill.key)
       );
-      console.log(`${target.name}이 종료 되었습니다`);
-    }, target.cooltime * 1000);
+      console.log(`${skill.name}이 종료 되었습니다`);
+    }, cooltimeDuration);
 
     // 실행할 프로세스
-    alert(`${target.cooltime}초 후에 실행됩니다.`);
+    // alert(`${target.cooltime}초 후에 실행됩니다.`);
   };
 
   return (
@@ -78,6 +110,38 @@ export default function Page() {
       <h1>Key Press Handler</h1>
       <p>Press key and then Enter to trigger a specific process.</p>
       <p>Current State: {isKeyPressed ? "Waiting for Enter" : "Idle"}</p>
+
+      <div>
+        <h2>Key Binding List</h2>
+        <ul className="flex gap-3">
+          {eventList.map((data) => (
+            <li key={data.key}>
+              <div className="flex gap-3">
+                {/* <h3>{data.name}</h3> */}
+                <div className="w-32 h-32 relative">
+                  <img
+                    className="w-full h-full object-cover brightness-80"
+                    src={data.image}
+                    alt={`${data.name} 이미지`}
+                  />
+                  <div
+                    className={`absolute inset-0 bg-gradient-to-tr from-transparent to-black opacity-50 rounded-full`}
+                    style={{
+                      maskImage: `conic-gradient(transparent ${
+                        100 - (data.remainingCooltime / data.cooltime) * 100
+                      }%, black 0)`,
+                      WebkitMaskImage: `conic-gradient(transparent ${
+                        100 - (data.remainingCooltime / data.cooltime) * 100
+                      }%, black 0)`,
+                    }}
+                  ></div>
+                </div>
+                {/* <div>({data.remainingCooltime}초)</div> */}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
